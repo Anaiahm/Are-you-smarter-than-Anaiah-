d86a484114b2b6b56982bb1cfd7b87f65345ca2a
Anaiahm <anaiahmaddox@gmail.com>
2024-07-18 14:32:23 -0400

    Added buttons and questions for category1

---
+++ data.js
@@ -0,0 +1,25 @@
+// const Ans1
+// const Ans2
+// const Ans3
+
+// playerChoice
+// corrAns
+// Message
+
+//                         //   Buttons!
+// const kmButtonElement = document.querySelector('#km-button');
+// kmButtonElement.addEventListener('click',() => {
+// console.log('I work!');
+// });
+// const rfButtonElement = document.querySelector('#rf-button');
+// rfButtonElement.addEventListener('click',() => {
+// console.log('I work!');
+// });
+// const pcButtonElement = document.querySelector('#pc-button');
+// pcButtonElement.addEventListener('click',() => {
+// console.log('I work!');
+// });
+// const backButtonElement = document.querySelector('#back-button');
+// kmButtonElement.addEventListener('click',() => {
+// console.log('I work!');
+// });
